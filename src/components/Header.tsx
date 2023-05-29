cd93fa8907f103c125e7fe707e3841695b140b0c
Anastasia <nastiakarachova@gmail.com>
2023-05-29 12:23:40 +0300

    feat: add links at header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,18 @@
 import { ReactComponent as Cart } from 'assets/icons/cart.svg'
+import { Link } from 'react-router-dom'
+import { routes } from 'routes/routes'
 
 export const Header = () => {
   return (
     <header className="bg-blue-900 py-6">
       <div className="container mx-auto flex justify-between items-center">
-        <span className="text-white font-medium text-2xl">Products</span>
-        <button className="relative">
+        <Link to={routes.products} className="text-white font-medium text-2xl">
+          Products
+        </Link>
+        <Link to={routes.cart} className="relative">
           <Cart className="w-6 h-6 fill-white" />
           <span className="bg-blue-500 rounded-full h-4 flex items-center justify-center text-white w-4 text-xs absolute -top-1 -right-3">5</span>
-        </button>
+        </Link>
       </div>
     </header>
   )
