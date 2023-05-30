533dcfcd52a0e51d3d1a2f3a3e67ca0ca2e1da9f
Anastasia <nastiakarachova@gmail.com>
2023-05-30 19:20:44 +0300

    fix: get products from api

--- src/modules/products/index.tsx
+++ src/modules/products/index.tsx
@@ -1,13 +1,15 @@
-import { products } from 'data.json'
+import { useProductsStore } from 'store'
+import { useEffect } from 'react'
 
 import { Product } from './components/Product'
 
 export const Products = () => {
-  return (
-    <section className="grid grid-cols-3 gap-5 py-8">
-      {products.map(product => (
-        <Product key={product.id} product={product} />
-      ))}
-    </section>
-  )
+  const { setProducts } = useProductsStore(store => store)
+  const { products } = useProductsStore(store => store)
+
+  useEffect(() => {
+    setProducts()
+  }, [])
+
+  return <section className="grid grid-cols-3 gap-5 py-8">{products.length > 0 ? products.map(product => <Product key={product.id} product={product} />) : null}</section>
 }
