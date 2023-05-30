4b7c10ae2c16800f8bdc2c7ee72b76d7184207ff
Anastasia <nastiakarachova@gmail.com>
2023-05-30 19:15:47 +0300

    fix: change types for button component

--- src/common/Button.tsx
+++ src/common/Button.tsx
@@ -1,13 +1,13 @@
 import { FC } from 'react'
 
-interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
   className?: string
 }
 
 export const Button: FC<ButtonProps> = ({ children, className, ...props }) => {
   return (
-    <button className={`bg-blue-500 text-white p-3 border-transparent border-[1px] rounded-xl font-bold transition-colors duration-300 hover:bg-white hover:text-blue-500  hover:border-blue-500 ${className}`} {...props}>
+    <button className={`bg-blue-500 text-white p-3 border-transparent border-[1px] rounded-xl font-bold transition-colors duration-300 hover:bg-white hover:text-blue-500  hover:border-blue-500  ${className}`} {...props}>
       {children}
     </button>
   )
