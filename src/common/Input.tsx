63dc406c90d0555665534833077d276e7138b722
Anastasia <nastiakarachova@gmail.com>
2023-05-30 19:15:59 +0300

    fix: add ref for input

--- src/common/Input.tsx
+++ src/common/Input.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react'
+import React, { forwardRef, InputHTMLAttributes, Ref } from 'react'
 
-export const Input: FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ ...props }) => {
-  return <input className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none" {...props} />
-}
+export const Input: React.FC<InputHTMLAttributes<HTMLInputElement>> = forwardRef((props, ref: Ref<HTMLInputElement>) => {
+  return <input ref={ref} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 outline-none" {...props} />
+})
